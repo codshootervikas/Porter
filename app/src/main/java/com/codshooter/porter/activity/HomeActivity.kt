228e25f788badaa967f37bf529af5e550c516099
saurabhthakur21 <148635862+anamikathhakur@users.noreply.github.com>
2024-12-01 16:17:57 +0530

    "fix some bugs"

--- app/src/main/java/com/codshooter/porter/activity/HomeActivity.kt
+++ app/src/main/java/com/codshooter/porter/activity/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.codshooter.porter.R
 import com.codshooter.porter.databinding.ActivityHomeBinding
 import com.codshooter.porter.fragments.AccountFragment
@@ -13,37 +14,48 @@ import com.codshooter.porter.fragments.OrdersFragment
 import com.codshooter.porter.fragments.PaymentsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.fragmentContainerView2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navController.navigate(R.id.homeFragment)
 
-        loadFragment(HomeFragment())
-
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> loadFragment(HomeFragment())
-                R.id.orders -> loadFragment(OrdersFragment())
-                R.id.coins -> loadFragment(CoinsFragment())
-                R.id.payments -> loadFragment(PaymentsFragment())
-                R.id.account -> loadFragment(AccountFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.ordersFragment -> {
+                    navController.navigate(R.id.ordersFragment)
+                    true
+                }
+                R.id.coinsFragment -> {
+                    navController.navigate(R.id.coinsFragment)
+                    true
+                }
+                R.id.paymentsFragment -> {
+                    navController.navigate(R.id.paymentsFragment)
+                    true
+                }
+                R.id.accountFragment -> {
+                    navController.navigate(R.id.accountFragment)
+                    true
+                }
                 else -> false
             }
-            true
         }
-    }
-
-    private fun loadFragment(fragment: Fragment): Boolean {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main, fragment)
-            .commit()
-        return true
 
     }
+
+
 }
 
